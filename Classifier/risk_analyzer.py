b47ed2b95d5ba0cc957594b1cf956e6b8a533949
Andrew Dennis <rewman55@yahoo.com>
2018-11-11 14:43:58 -0500

    add some code changes and ROC chart

--- Classifier/risk_analyzer.py
+++ Classifier/risk_analyzer.py
@@ -9,6 +9,7 @@ from sklearn.metrics import classification_report, confusion_matrix, accuracy_sc
 import xgboost as xgb
 
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sns
 
 from group_data import data, group_main
@@ -65,12 +66,10 @@ df_importances['importance'] = df_importances['importance'].astype(float)
 
 imp= sns.barplot(x='variable',y='importance', data=df_importances, palette='Blues_d')
 imp.set_xticklabels(imp.get_xticklabels(), rotation=30)
+plt.show()
 
 
 """Visualization of Data"""
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-
 fig = plt.figure()
 ax = fig.add_subplot(111, projection = '3d')
 
@@ -84,7 +83,24 @@ ax.scatter(X,Y, Z, c='r', marker='+')
 ax.set_xlabel('Sales Beat')
 ax.set_ylabel('EPS Beat')
 ax.set_zlabel('Mean Positivity')
+plt.show()
 
 
 
-# plt.show()
+"""Calculate the fpr and tpr for all thresholds of the classification."""
+probs = regressor.predict_proba(X_test)
+preds = probs[:,1]
+fpr, tpr, threshold = metrics.roc_curve(y_test, preds)
+roc_auc = metrics.auc(fpr, tpr)
+
+"""ROC CURVE Plot"""
+plt.title('Receiver Operating Characteristic')
+plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
+plt.legend(loc = 'lower right')
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0, 1])
+plt.ylim([0, 1])
+plt.ylabel('True Positive Rate')
+plt.xlabel('False Positive Rate')
+plt.show()
+
