f0047c81d2702ac54fd9f115b192f35c7f25bfb2
Andrew Dennis <rewman55@yahoo.com>
2018-11-14 19:24:08 -0500

    second push to repo add other files used in thesis

--- Classifier/classifier.py
+++ Classifier/classifier.py
@@ -1,14 +1,20 @@
+""" This script is meant to utilized the sentiment lexicon.
+This will count each negative word in each sentence. 
+No words have been given a weight just a simple count. 
 
+Author: Andrew Dennis
+"""
 import numpy as np
 import pandas as pd
 import re
 
-from sentiment import *
+from sentiment import positive, negative, uncertainty
 
 df = pd.read_csv(r'D:\MS Data Science Files\Thesis\model_data.csv')
 
 
 def neg_count(phrase):
+	"""Negative counter."""
 	neg_count= 0
 	for word in re.split(' ',phrase):
 		if word.lower() in negative:
@@ -17,23 +23,26 @@ def neg_count(phrase):
 
 
 def pos_count(phrase):
+	"""Positive counter."""
 	pos_count= 0
 	for word in re.split(' ',phrase):
 		if word.lower() in positive:
 			pos_count = pos_count + 1
 	return pos_count
 
+
 def unc_count(phrase):
+	"""Uncertainty counter."""
 	unc_count= 0
 	for word in re.split(' ',phrase):
 		if word.lower() in uncertainty:
 			unc_count = unc_count + 1
 	return unc_count
 
+
 df['NEG'] = df['tokenized'].apply(neg_count)
 df['POS'] = df['tokenized'].apply(pos_count)
 df['UNC'] = df['tokenized'].apply(unc_count)
 
 
-print df.head()
 # df.to_csv('D:\\MS Data Science Files\\Thesis\\classified_data.csv')
--- Classifier/risk_analyzer.py
+++ Classifier/risk_analyzer.py
@@ -1,6 +1,7 @@
+""" Author: Andrew Dennis"""
+
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
@@ -22,6 +23,7 @@ df= group_main(df_sent, earn_df)
 
 df.to_csv('D:\\MS Data Science Files\\Thesis\\final_df.csv')
 
+
 X = df[[
 		'sum_neg', 
 		'sum_unc', 
@@ -33,11 +35,11 @@ X = df[[
 		'eps_sur',
 		'sales_sur'
 		]]
-
 y = df[['risk']]
 
+
 """Split data for testing algorithms""" 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True)  
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, shuffle= True)  
 
 
 """Random Forrest"""
@@ -49,10 +51,9 @@ y_pred = regressor.predict(X_test)
 """Random Forrest Results"""
 print(confusion_matrix(y_test,y_pred.round()))
 print(' ')  
-print('Top Three Variables    ')
+print('Results    ')
 print(classification_report(y_test,y_pred.round()))  
 print('Accuracy %:' , accuracy_score(y_test, y_pred.round())*100)  
-
 
 
 """Forest Features"""
@@ -60,18 +61,17 @@ importances = dict(zip(X_train.columns, regressor.feature_importances_))
 
 imp_df = pd.DataFrame(importances.items(), columns=['variable','importance'])
 
-
 df_importances = imp_df.sort_values(by=['importance'], ascending=False)
 df_importances['importance'] = df_importances['importance'].astype(float)
 
-imp= sns.barplot(x='variable',y='importance', data=df_importances, palette='Blues_d')
-imp.set_xticklabels(imp.get_xticklabels(), rotation=30)
+imp= sns.barplot(x= 'variable',y= 'importance', data= df_importances, palette= 'Blues_d')
+imp.set_xticklabels(imp.get_xticklabels(), rotation= 30)
 plt.show()
 
 
 """Visualization of Data"""
 fig = plt.figure()
-ax = fig.add_subplot(111, projection = '3d')
+ax = fig.add_subplot(111, projection= '3d')
 
 X = df['sales_sur'].astype(float)
 Y = df['eps_sur'].astype(float)
@@ -79,12 +79,11 @@ Z = df['mean_pos'].astype(float)
 
 ax.set_ylim3d(-100,200)
 
-ax.scatter(X,Y, Z, c='r', marker='+')
+ax.scatter(X,Y, Z, c= 'r', marker= '+')
 ax.set_xlabel('Sales Beat')
 ax.set_ylabel('EPS Beat')
 ax.set_zlabel('Mean Positivity')
 plt.show()
-
 
 
 """Calculate the fpr and tpr for all thresholds of the classification."""
@@ -93,14 +92,14 @@ preds = probs[:,1]
 fpr, tpr, threshold = metrics.roc_curve(y_test, preds)
 roc_auc = metrics.auc(fpr, tpr)
 
+
 """ROC CURVE Plot"""
 plt.title('Receiver Operating Characteristic')
-plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
-plt.legend(loc = 'lower right')
+plt.plot(fpr, tpr, 'b', label= 'AUC = %0.2f' % roc_auc)
+plt.legend(loc= 'lower right')
 plt.plot([0, 1], [0, 1],'r--')
 plt.xlim([0, 1])
 plt.ylim([0, 1])
 plt.ylabel('True Positive Rate')
 plt.xlabel('False Positive Rate')
 plt.show()
-
